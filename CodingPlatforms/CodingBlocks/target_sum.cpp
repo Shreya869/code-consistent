be2a0b7368bcf051f7708495813273e9e2b78693
Shreya <62697529+Shreya869@users.noreply.github.com>
2020-08-21 00:34:35 +0530

    Create target_sum.cpp

---
+++ CodingPlatforms/CodingBlocks/target_sum.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+void target_sum(int a[], int target, int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			if(a[i]+a[j]==target)
+			{
+				int left=i;
+				int right=j;
+				if(a[i]<a[j])
+				{
+					cout<<a[i]<<" and "<<a[j];
+
+				}
+				else{
+					cout<<a[j]<<" and "<<a[i];
+				}
+				cout<<endl;
+				
+			}
+			
+
+		}
+	}
+}
+int main() {
+	int n;
+	cin>>n;
+	
+	int a[1000];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+
+	int target;
+	cin>>target;
+
+	target_sum(a,target,n);
+
+	return 0;
+}
